5e1805a7a1fc82fd0db805e7042fead5055364df
skrabacher <37253871+skrabacher@users.noreply.github.com>
2020-02-19 09:19:03 -0800

    added validation and associations to models

--- models/course.js
+++ models/course.js
@@ -1,15 +1,109 @@
-//  npx sequelize model:create --name Course --attributes userId:integer,title:string,description:text,estimatedTime:string,materialsNeeded:string
 'use strict';
-module.exports = (sequelize, DataTypes) => {
-  const Course = sequelize.define('Course', {
-    userId: DataTypes.INTEGER,
-    title: DataTypes.STRING,
-    description: DataTypes.TEXT,
-    estimatedTime: DataTypes.STRING,
-    materialsNeeded: DataTypes.STRING
-  }, {});
-  Course.associate = function(models) {
-    // associations can be defined here
-  };
+//defines and exports the course model
+const Sequelize = require('sequelize');
+
+module.exports = (sequelize) => {
+  class Course extends Sequelize.Model {}
+  Course.init({
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+    },
+// userId (id from the Users table)
+
+    // userId: {
+    //     type: Sequelize.INTEGER,
+    //     allowNull: false,
+    //     validate: {
+    //         notNull: {
+    //           msg: 'Please provide a value for "userId"',
+    //         },
+    //         notEmpty: {
+    //           msg: 'Please provide a value for "userId"',
+    //         },
+    //       },
+    // },
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            notNull: {
+              msg: 'Please provide a value for "title"',
+            },
+            notEmpty: {
+              msg: 'Please provide a value for "title"',
+            },
+          },
+    },
+    description: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+        validate: {
+            notNull: {
+              msg: 'Please provide a value for "description"',
+            },
+            notEmpty: {
+              msg: 'Please provide a value for "description"',
+            },
+          },
+    },
+    estimatedTime: {
+        type: Sequelize.STRING,
+        allowNull: true
+    },
+    materialsNeeded: {
+        type: Sequelize.STRING,
+        allowNull: true
+    },
+  }, { sequelize });
+
+  Course.associate = (models) => {
+    // Add a one-to-one association between the Course and User model
+    models.Course.belongsTo(models.User, { 
+      as: "instructor", //remove this it causes issues?... shoudl be renaming the column header in the db
+      foreignKey: {
+        fieldName: 'instructorPersonId',
+        allowNull: false,
+      },
+    });
+  }
   return Course;
 };
+
+
+
+// Model Spec Notes:
+    // Course
+    // id (Integer, primary key, auto-generated)
+    // userId (id from the Users table)
+    // title (String)
+    // description (Text)
+    // estimatedTime (String, nullable)
+    // materialsNeeded (String, nullable)
+
+
+
+
+
+
+
+
+//  npx sequelize model:create --name Course --attributes userId:integer,title:string,description:text,estimatedTime:string,materialsNeeded:string
+
+      // 'use strict';
+
+      // module.exports = (sequelize, DataTypes) => {
+      //   const Course = sequelize.define('Course', {
+      //     userId: DataTypes.INTEGER,
+      //     title: DataTypes.STRING,
+      //     description: DataTypes.TEXT,
+      //     estimatedTime: DataTypes.STRING,
+      //     materialsNeeded: DataTypes.STRING
+      //   }, {});
+      //   Course.associate = function(models) {
+      //     // associations can be defined here
+      //   };
+      //   return Course;
+      // };
--- models/user.js
+++ models/user.js
@@ -1,14 +1,102 @@
-// npx sequelize model:create --name User --attributes firstName:string,lastName:string,emailAddress:string,password:string
+//defines and exports the user model
+
 'use strict';
-module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    emailAddress: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {});
-  User.associate = function(models) {
-    // associations can be defined here
+
+const Sequelize = require('sequelize');
+
+module.exports = (sequelize) => {
+  class User extends Sequelize.Model {}
+  User.init({
+    id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    firstName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            notNull: {
+              msg: 'Please provide a value for "firstName"',
+            },
+            notEmpty: {
+              msg: 'Please provide a value for "firstName"',
+            },
+          },
+    },
+    lastName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            notNull: {
+              msg: 'Please provide a value for "lastName"',
+            },
+            notEmpty: {
+              msg: 'Please provide a value for "lasstName"',
+            },
+          },
+    },
+    emailAddress: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            notNull: {
+              msg: 'Please provide a value for "emailAddress"',
+            },
+            notEmpty: {
+              msg: 'Please provide a value for "emailAddress"',
+            },
+          },
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            notNull: {
+              msg: 'Please provide a value for "password"',
+            },
+            notEmpty: {
+              msg: 'Please provide a value for "password"',
+            },
+          },
+    },
+  }, { sequelize });
+
+  User.associate = (models) => {
+    // adds one to many association with Course (one user, many coursess)
+    models.User.hasMany(models.Course, { 
+      as: "instructor", //remove this it causes issues?... shoudl be renaming the column header in the db
+      foreignKey: {
+        fieldName: 'instructorPersonId',
+        allowNull: false,
+      },
+    });
   };
+
   return User;
 };
+
+
+//Model Spec Notes:
+// User
+// id (Integer, primary key, auto-generated)
+// firstName (String)
+// lastName (String)
+// emailAddress (String)
+// password (String)
+
+// npx sequelize model:create --name User --attributes firstName:string,lastName:string,emailAddress:string,password:string
+    // 'use strict';
+    // module.exports = (sequelize, DataTypes) => {
+    //   const User = sequelize.define('User', {
+    //     firstName: DataTypes.STRING,
+    //     lastName: DataTypes.STRING,
+    //     emailAddress: DataTypes.STRING,
+    //     password: DataTypes.STRING
+    //   }, {});
+    //   User.associate = function(models) {
+    //     // associations can be defined here
+    //   };
+    //   return User;
+    // };
